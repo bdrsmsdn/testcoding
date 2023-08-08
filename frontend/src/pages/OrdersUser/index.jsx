fa8abcb9439dbceb59d85e175ca24d4a30abed37
bdrsmsdn <badrasam7@gmail.com>
2023-08-09 00:15:20 +0700

    Fix minor bugs

--- frontend/src/pages/OrdersUser/index.jsx
+++ frontend/src/pages/OrdersUser/index.jsx
@@ -321,7 +321,7 @@ const OrdersUser = () => {
                     className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     onClick={() => handleAddOrder(storedData)}
                   >
-                    Add Car
+                    Add Order
                   </button>
                   <button
                     type="button"
